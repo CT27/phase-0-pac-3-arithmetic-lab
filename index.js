5102ee5af809f402314c567856954f1760c1a5c0
Candice Thomason <candice.thomason@gmail.com>
2024-02-27 17:56:52 +1100

    add 10 of 15 tests

---
+++ index.js
@@ -0,0 +1,44 @@
+// Basic math functions
+
+1 + 80; //=> 81
+60 - 40; //=> 20
+2 * 3.4; //=> 6.8
+5.0 / 2.5; //=> 2v
+
+let number = 5;
+
+number++; //=> 5... hmmmm
+
+number; //=> 6 -- the number was incremented after it was evaluated
+
+number--; //=> 6
+
+number; //=> 5
+
+// Define the function & call the values via return
+function add(a,b){
+return a+b;
+}
+
+function subtract(a,b){
+return a-b;
+}
+
+function multiply(a,b){
+return a*b;
+}
+
+function divide(a,b) {
+return a/b;
+}
+
+function increment(n){
+return n+1;
+}
+
+
+function decrement(n) {
+return n-1;
+}
+
+
